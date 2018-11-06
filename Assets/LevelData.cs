485f7cd9a36aea825938a4c3187777206117c460
Jack Christ <jchrist0922@gmail.com>
2018-11-05 20:10:07 -0800

    last commit

---
+++ Assets/LevelData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Data
+{
+    public string levelname;
+    public bool unlocked;
+}
+public class LevelData : MonoBehaviour {
+    public Data[] data;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < GameObject.FindGameObjectWithTag("levels").transform.GetChild(0).childCount; i++)
+        {
+
+            GameObject.FindGameObjectWithTag("levels").transform.GetChild(0).GetChild(i).GetComponent<Leveldot>().SetCanEnter(data[i].unlocked);
+            
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
