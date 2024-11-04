d5ec78aa69ada66c49ce4136c52279d8959fa013
RandomiaGaming <randomiagaming@gmail.com>
2024-11-04 14:02:25 -0800

    Backup commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelManager : MonoBehaviour {
+    public Color unlocked;
+    public Color locked;
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(this);
+        if (GameObject.FindGameObjectsWithTag("LevelManager").Length < 2)
+        {
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "levelselect")
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+           transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+    public	void Win()
+    {
+        print("won");   
+        for (int i = 0; i < transform.GetChild(0).childCount; i++)
+        {
+            if(transform.GetChild(0).GetChild(i).GetComponent<Leveldot>().levelname == SceneManager.GetActiveScene().name)
+            {
+                print("foundchild");
+                transform.GetChild(0).GetChild(i).GetComponent<Leveldot>().Complete();
+            }
+        }
+        print("loadingscene");
+        SceneManager.LoadSceneAsync("levelselect");
+    }
+}
---
+++ Assets/square.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class square : MonoBehaviour {
+    public float wait = 10;
+    private float current;
+    public Vector2 jump;
+    public Transform player;
+	// Use this for initialization
+	void Start () {
+        current = wait;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Mathf.Sign(jump.x) != Mathf.Sign(transform.position.x - player.position.x))
+        {
+            jump.x *= -1;
+        }
+        current -= Time.deltaTime;
+        if(wait <= 0)
+        {
+            Jump();
+            current = wait;
+        }
+	}
+    public void Jump()
+    {
+       
+        GetComponent<Rigidbody2D>().velocity = jump;
+    }
+}
